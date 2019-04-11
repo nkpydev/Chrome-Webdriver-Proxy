461d3e4318d0fa67d3d01ccfcd6c0d31f5507133
Nitin K <39520442+nkpydev@users.noreply.github.com>
2019-04-11 21:53:42 +0530

    Test scenario for testing extension clean up added

--- test_chrome_proxy.py
+++ test_chrome_proxy.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from utility.chrome_proxy_driver_setup import ChromeProxySetup as CPS
 import time
+from utility.extension_clean_up import cleanUp
 
 b = CPS() # No Proxy
 c = CPS('127.0.0.1',5000,'proxy_auth','proxy_pwd') # With Proxy
@@ -14,3 +15,4 @@ driver2.maximize_window()
 driver2.get('http://google.com')
 time.sleep(5)
 driver2.close()
+cleanUp(True).clean_extensions()
